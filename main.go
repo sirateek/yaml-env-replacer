45142265bbcc0928b813e49bae857cf7a5a88b9e
Siratee K <sirateek@gmail.com>
2022-09-16 00:23:32 +0700

    fix: Make non replacable syntax also save to target file

--- main.go
+++ main.go
@@ -32,39 +32,37 @@ func main() {
 	logger.Debug(searchResult)
 	if len(searchResult) == 0 {
 		logger.Warn("There is no replacing syntax `${...}` in the config file. Kindly Exiting... :-)")
-		os.Exit(0)
-		return
-	}
-
-	// Read Env file.
-	err = ReadEnvConfigFile(path.EnvFile)
-	if err != nil {
-		os.Exit(1)
-		return
-	}
-
-	// Replace the `${NAME}` by using RegExp.
-	for _, value := range searchResult {
-		// Substring value to remove ${ and }
-		lengthOfEnvKey := len(value)
-		envKey := value[2 : lengthOfEnvKey-1]
-		logger.Debug("Getting value for key: ", envKey)
-
-		//  Get env from viper.
-		envValue := viper.GetString(envKey)
-		logger.Debug("Value is: ", envValue)
-		if envValue == "" {
-			logger.Error("There is no replacement for this key: ", envKey)
+	} else {
+		// Read Env file.
+		err = ReadEnvConfigFile(path.EnvFile)
+		if err != nil {
 			os.Exit(1)
 			return
 		}
-		logger.Info("Replacing the ", value, " value with ", envValue)
 
-		// Replace that with value from viper.
-		localMatcher, _ := regexp.Compile(value)
-		configDataString = localMatcher.ReplaceAllString(configDataString, envValue)
+		// Replace the `${NAME}` by using RegExp.
+		for _, value := range searchResult {
+			// Substring value to remove ${ and }
+			lengthOfEnvKey := len(value)
+			envKey := value[2 : lengthOfEnvKey-1]
+			logger.Debug("Getting value for key: ", envKey)
+
+			//  Get env from viper.
+			envValue := viper.GetString(envKey)
+			logger.Debug("Value is: ", envValue)
+			if envValue == "" {
+				logger.Error("There is no replacement for this key: ", envKey)
+				os.Exit(1)
+				return
+			}
+			logger.Info("Replacing the ", value, " value with ", envValue)
+
+			// Replace that with value from viper.
+			localMatcher, _ := regexp.Compile(value)
+			configDataString = localMatcher.ReplaceAllString(configDataString, envValue)
+		}
+		logger.Info("Done replacing.")
 	}
-	logger.Info("Done replacing.")
 
 	// Write to file.
 	err = os.WriteFile(path.OutputFile, []byte(configDataString), 0777)
